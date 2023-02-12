a2ac02879c347d8dbecd8b17e93ae606febc57d5
ARoman87 <angel427roman@gmail.com>
2023-02-12 15:44:42 -0500

    implementing cors

--- src/app.ts
+++ src/app.ts
@@ -10,15 +10,23 @@ import MongoStore from "connect-mongo";
 import { requiresAuth } from "./middleware/auth";
 import cors from "cors"
 
+const corsOptions = {
+    origin: "*",
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+
 
 
 const app = express();
+
+app.use(cors(corsOptions))
 
 app.use(morgan("dev"));
 
 app.use(express.json());
 
-app.use(cors)
+
 
 app.use(session({
     secret: env.SESSION_SECRET,
